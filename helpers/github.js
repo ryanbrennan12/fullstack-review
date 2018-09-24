464283433c436e9aa7a04a2d1de22b2de001a938
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-09-24 13:39:45 -0700

    [WIP] helper fetching data from API

--- helpers/github.js
+++ helpers/github.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const config = require('../config.js');
 
+console.log('This is my Token brah: ', config.TOKEN)
 let getReposByUsername = (username) => {
   
   // TODO - Use the request module to request repos for a specific
@@ -9,13 +10,18 @@ let getReposByUsername = (username) => {
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
-}
+  request(options, (err, res, body) => {
+    console.log('this is the BODY in helpers', body)
+    //save my DB info here?
+  })
 
+}
+getReposByUsername('ryanbrennan12')
 module.exports.getReposByUsername = getReposByUsername;
