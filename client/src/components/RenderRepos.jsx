ba716f4f5f1c292595f134277df6bc1b744845fa
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-09-25 22:38:57 -0700

    [WIP] -fixing RenderRepo.jsx

--- client/src/components/RenderRepos.jsx
+++ client/src/components/RenderRepos.jsx
@@ -11,38 +11,4 @@ const RenderRepos = (props) => (
 
 
 
-
-
-
-
-
-// var VideoList = ({videos, handleVideoListEntryTitleClick}) => (
-//   <div className="video-list">
-//     {videos.map((video) =>
-//       <VideoListEntry
-//         key={video.etag}
-//         video={video}
-//         handleVideoListEntryTitleClick={handleVideoListEntryTitleClick}
-//       />
-//     )}
-//   </div>
-// );
-
-// var VideoList = (props) => (
-//   <div className="video-list">
-//     {/* <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
-//     {props.videos.map(video =>
-//       <VideoListEntry video={video} onClick={props.onClick}/>
-//     )}
-
-//   </div>
-// );
-
-// const RenderRepos = (props) => {
-//   {props.repos.map((repo) => {
-//      return (
-//       <div> </div>
-//      ); 
-//   })}
-// }
 export default RenderRepos;
