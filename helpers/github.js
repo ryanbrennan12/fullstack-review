62fc6c8f9e93bfc3608cedde3a4ce909f2ddbd21
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-09-27 16:16:58 -0700

    [BMR] - more heroku

--- helpers/github.js
+++ helpers/github.js
@@ -1,6 +1,6 @@
 const request = require('request');
-const config = require('../config.js');
-// const mongoose = require('mongoose');
+// const config = require('../config.js');
+
 const db = require('../database/index')
 // const Repo = mongoose.model('Repo');
 
@@ -9,13 +9,14 @@ let getReposByUsername = (username, cb) => {
 // console.log('what is the type', typeof(username))  PASSED AS A STRING
   // TODO - Use the request module to request repos for a specific
   // user from the github API
+  let configToken = process.env.TOKEN; 
 
   let options = {
     method: 'GET',
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${configToken}`
     }
   };
   
